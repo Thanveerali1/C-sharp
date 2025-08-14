f071668d8631402c2e81191beb3e455ea6e878d3
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2025-08-14 10:36:38 +0530

    Add files via upload

---
+++ course/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+class Course
+{
+    protected void ShowDetails()
+    {
+        Console.WriteLine("Course Name: Introduction to C#");
+        Console.WriteLine("Duration: 4 weeks");
+    }
+}
+
+class OnlineCourse : Course
+{
+    public void DisplayCourse()
+    {
+     
+        ShowDetails();
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        OnlineCourse oc = new OnlineCourse();
+
+        oc.DisplayCourse();
+        Console.ReadLine();
+    }
+}
