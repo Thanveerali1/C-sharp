a6640ccd90033e7004d5dd63130d7d9485a5ef62
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2025-09-25 10:56:26 +0530

    Add files via upload

---
+++ Stringwrite/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StringWriter sw = new StringWriter())
+        {
+            sw.WriteLine("Hello, World!");
+            sw.WriteLine("This is a StringWriter example.");
+            Console.WriteLine("StringWriter contents:");
+            Console.WriteLine(sw.ToString());
+        }
+    }
+}
---
+++ writeafile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "example1.txt";
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("Hello, World!");
+            writer.WriteLine("This is written using StreamWriter.");
+        }
+        Console.WriteLine("File written using StreamWriter.");
+    }
+}
