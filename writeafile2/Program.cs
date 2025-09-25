0bd90bbc3b8670579e0c12abf7954d8cec9b1e5b
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2025-09-25 10:57:11 +0530

    Add files via upload

---
+++ writeafile2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "example2.txt";
+        string content = "Hello, World!\nThis is written using File.WriteAllText.";
+        File.WriteAllText(filePath, content);
+        Console.WriteLine("File written using File.WriteAllText.");
+    }
+}
