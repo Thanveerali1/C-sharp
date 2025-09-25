94bf3925e04f410bce281c4065f421b59ad18121
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2025-09-25 10:29:05 +0530

    Add files via upload

---
+++ pascaltriangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int value = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(value + " ");
+                    
+                    value = value * (i - j) / (j + 1);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
